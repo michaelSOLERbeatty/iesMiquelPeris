57fd69f5e57697dac9e5dfd5b051abe2ef2fdd64
michaelSOLERbeatty <michael.soler.beatty@gmail.com>
2026-05-13 06:36:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function horarioPermitido() {
   const hora = ahora.getHours();
   const minuto = ahora.getMinutes();
 
-  return hora === 6 && minuto >= 0 && minuto <= 20;
+  return hora === 6 && minuto >= 0 && minuto <= 50;
 }
 
 function reservar(material) {
